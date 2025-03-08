71bba27445a53fd9c1ebc4198c7ac9abb83ef882
Carlos1408 <carlos.claure.vargas@gmail.com>
2025-03-08 09:54:39 -0400

    added ticket deleting

--- backend/src/controllers/ticket.controller.js
+++ backend/src/controllers/ticket.controller.js
@@ -17,4 +17,20 @@ const create = async (req, res) => {
   }
 };
 
-module.exports = { create };
+const deleteById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const ticket = await prisma.ticket.findFirst({ where: { id } });
+    if (!id) throw new Error("Missing id");
+    if (!ticket) throw new Error("Not found");
+    await prisma.ticket.delete({
+      where: { id },
+    });
+    res.sendStatus(204);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ error });
+  }
+};
+
+module.exports = { create, deleteById };
--- backend/src/routes/ticket.routes.js
+++ backend/src/routes/ticket.routes.js
@@ -4,5 +4,6 @@ const ticketController = require("../controllers/ticket.controller");
 const router = Router();
 
 router.post("/", ticketController.create);
+router.delete("/:id", ticketController.deleteById);
 
 module.exports = router;
